3404b04be4821d05d5f2a370144b07ca5d85c284
Akash Gogoi <gogoiakash09@gmail.com>
2024-01-09 20:34:29 +0530

    add different routes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <h1>Brick-Bazaar</h1>
+    <h1>Brick-Blocks</h1>
   )
 }
---
+++ app/products/page.jsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <h1>Hello from products page</h1>
+  )
+}
+
+export default page
